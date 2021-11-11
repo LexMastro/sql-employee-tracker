d4812fc225f8dffaee0f75c9605f889ff6eab611
Alexis Mastrodomenico <86946841+LexMastro@users.noreply.github.com>
2021-11-11 11:04:30 +1100

    started server.js

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'r00tpassword',
+    }
+);
+
+// connect to mysql
+db.connect(err => {
+    if (err) {
+        throw err
+    }
+    console.log('MySQL Connected!')
+});
+
+//create database
+app.get('/createdb', (req, res) => {
+    const sql = `CREATE DATABASE nodemysql`;
+    db.query(sql, err => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Success, database created!',
+        });
+    });
+});
+
+//Create Table
+app.get('/createdemployee', (req, res) => {
+    const sql = `CREATE TABLE employee(id int AUTO_INCREMENT, first_name VARCHAR(30),last_name VARCHAR(30), role_id INT, manager_id INT)`;
+    db.query(sql, err => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Success, employee table created!',
+        });
+    });
+});
+
+//Insert employee
+app.get('/employee1', (req, res) => {
+    const post = { first_name: 'Alexis', last_name: 'Mastro', role_id: 'Web Developer', manager_id: 1 };
+    const sql = 'INSERT INTO employee SET'
+    db.query(sql, post, err => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Success, employee added!',
+        });
+    });
+});
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
